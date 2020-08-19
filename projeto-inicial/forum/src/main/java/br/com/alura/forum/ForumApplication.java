d28a454f0f64d26c7d90c198d88bb8de487c94c7
Matheus Schreiner Homrich da Silva <matheus-homrich@NTCNOTE81.ntconsult.net>
2020-08-19 18:59:03 -0300

    correcao projeto

--- projeto-inicial/forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ projeto-inicial/forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -1,13 +1,13 @@
-package com.ntconsult.demo;
+package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ForumApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ForumApplication.class, args);
 	}
 
 }
--- projeto-inicial/forum/src/main/java/controllers/HelloWorldController.java
+++ projeto-inicial/forum/src/main/java/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.ntconsult.demo.controller;
+package controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
