b070473cb0bbe077c7ef937c1966bed884e514d8
Matheus Schreiner Homrich da Silva <matheus-homrich@NTCNOTE81.ntconsult.net>
2020-08-27 14:40:46 -0300

    chegando na ultima versao

--- Spring_REST-master/projeto-inicial/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ Spring_REST-master/projeto-inicial/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -59,6 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     //recursos estaticos(js, css, imagens)
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 }
